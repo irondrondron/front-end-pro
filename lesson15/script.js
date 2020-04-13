b8a9e3c1270631abc8fd67fcd578ec52a68b5bc9
Andrey Androsov <irondrondron@gmail.com>
2020-04-13 10:41:12 +0300

    change values

--- lesson15/script.js
+++ lesson15/script.js
@@ -1,13 +1,13 @@
 var barbarian = new MilitaryResource(title = "Barbarian", health = 48, mana = 25);
 var demon = new MilitaryResource(title = "Demon", health = 120, mana = 98);
-var diablo = new MilitaryResource(title = "Diablo", health = 120, mana = 98);
-var fallenShaman = new MilitaryResource(title = "Fallen Shaman", health = 120, mana = 98);
-var fallen = new MilitaryResource(title = "Fallen", health = 120, mana = 98);
-var beast = new MilitaryResource(title = "Beast", health = 120, mana = 98);
-var goatman = new MilitaryResource(title = "Goatman", health = 120, mana = 98);
-var skeletonMage = new MilitaryResource(title = "Skeleton Mage", health = 120, mana = 98);
-var sorceress = new MilitaryResource(title = "Sorceress", health = 120, mana = 98);
-var wraith = new MilitaryResource(title = "Wraith", health = 120, mana = 98);
+var diablo = new MilitaryResource(title = "Diablo", health = 298, mana = 400);
+var fallenShaman = new MilitaryResource(title = "Fallen Shaman", health = 20, mana = 35);
+var fallen = new MilitaryResource(title = "Fallen", health = 6, mana = 14);
+var beast = new MilitaryResource(title = "Beast", health = 60, mana = 28);
+var goatman = new MilitaryResource(title = "Goatman", health = 56, mana = 38);
+var skeletonMage = new MilitaryResource(title = "Skeleton Mage", health = 20, mana = 54);
+var sorceress = new MilitaryResource(title = "Sorceress", health = 25, mana = 70);
+var wraith = new MilitaryResource(title = "Wraith", health = 38, mana = 110);
 
 var cards = document.getElementById('cards');
 
