601f837f79561d8efcb181e3f1afa0cd923e759d
Andrey Androsov <irondrondron@gmail.com>
2020-04-23 20:20:04 +0300

    app with promises

--- covid19/ajax.js
+++ covid19/ajax.js
@@ -1,23 +1,23 @@
-function request(method, url, success, error) {
-  var xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-
-  xhr.open(method, url);
-  xhr.addEventListener('load', function () {
-    if (success) {
-      success(JSON.parse(this.responseText))
-    }
+function request(method, url) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+  
+    xhr.open(method, url);
+    xhr.addEventListener('load', function () {
+        resolve(JSON.parse(this.responseText))
+    });
+    xhr.addEventListener('error', reject);
+    xhr.setRequestHeader("x-rapidapi-host", "covid-193.p.rapidapi.com");
+    xhr.setRequestHeader("x-rapidapi-key", "9b4cd80a8emsh009c6e5e4d00bf6p196305jsn8028d8d6647f");
+  
+    xhr.send();
+    return xhr;
   });
-  xhr.addEventListener('error', error);
-  xhr.setRequestHeader("x-rapidapi-host", "covid-193.p.rapidapi.com");
-  xhr.setRequestHeader("x-rapidapi-key", "9b4cd80a8emsh009c6e5e4d00bf6p196305jsn8028d8d6647f");
-
-  xhr.send();
-  return xhr;
 }
 
-function get(url, success, error) {
-  return request('GET', url, success || function () {}, error || function () {});
+function get(url) {
+  return request('GET', url);
 }
 
 function serialize(obj) {
--- covid19/script.js
+++ covid19/script.js
@@ -1,27 +1,57 @@
 var cards = document.getElementById("cards");
 var form = document.getElementById("filter-form");
-// var input = form.elements.countryName;
 
-function requestCountries(filter, success) {
-  get("https://covid-193.p.rapidapi.com/statistics/?" + serialize(filter), function (data) {
-    var arrayOfCards = data.response.map(function (card) {
-      var c = new CountryCard(card.country, card.cases.new, card.cases.active, card.cases.critical, card.cases.recovered,
-        card.cases.total, card.deaths.new, card.deaths.total, card.tests.total)
-      return c;
-    });
-
-    success(arrayOfCards);
+function requestCountries() {
+  return get("https://covid-193.p.rapidapi.com/statistics/").then(function (
+    data
+  ) {
+    return data.response.map((e) => e.country.toLowerCase());
   });
 }
 
-form.elements.countryName.addEventListener('input', debounce(function () {
-  requestCountries({
-      country: this.value
-    },
-    function (arrayOfCards) {
-      cards.innerHTML = "";
-      arrayOfCards.forEach(function (card) {
-        card.render(cards);
-      });
+function requestStatistics(filter) {
+  return get(
+    "https://covid-193.p.rapidapi.com/statistics/?" + serialize(filter)
+  ).then(function (data) {
+    return data.response.map(function (card) {
+      var c = new CountryCard(
+        card.country,
+        card.cases.new,
+        card.cases.active,
+        card.cases.critical,
+        card.cases.recovered,
+        card.cases.total,
+        card.deaths.new,
+        card.deaths.total,
+        card.tests.total
+      );
+      return c;
     });
-}, 200));
+  });
+}
+
+form.elements.countryName.addEventListener(
+  "input",
+  debounce(function () {
+    if (this.value !== "") {
+      var self = this;
+      requestCountries().then(function (arrayOfCountries) {
+        var countriesStartingWith = arrayOfCountries.filter((el) =>
+          el.startsWith(`${self.value}`, 0)
+        );
+        cards.innerHTML = "";
+        for (let i = 0; i < countriesStartingWith.length; i++) {
+          requestStatistics({
+            country: countriesStartingWith[i],
+          }).then(function (arrayOfCards) {
+            arrayOfCards.forEach(function (card) {
+              card.render(cards);
+            });
+          });
+        }
+      });
+    } else {
+      cards.innerHTML = "";
+    }
+  }, 200)
+);
